61e2a0b9d34ba71767ecbfef41ad386121ef46a5
sadmanador <sadman.ador@gmail.com>
2023-02-15 15:28:05 +0600

    64-3 working

--- src/App.js
+++ src/App.js
@@ -1,34 +1,55 @@
-import './App.css';
-import { useEffect, useState } from 'react';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-
-  const [users, setUsers]= useState([])
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:5000/users")
-    .then(res => res.json())
-    .then(data => setUsers(data))
-  },[])
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
 
   const handleFormInput = (event) => {
     event.preventDefault();
-  };
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const user = { name, email };
+    console.log(user);
 
+    //sending user to server
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        const newUsers = [...users, data];
+        setUsers(newUsers);
+      })
+      .catch((err) => console.error(err));
+    event.target.reset();
+  };
 
   return (
     <div className="App">
-     <form onSubmit={handleFormInput}>
-      <input type="text" placeholder='Name' name='name'/>
-      <input type="email" placeholder='Email' name='email'/>
-      <button type='submit'>Submit</button>
-     </form>
+      <form onSubmit={handleFormInput}>
+        <input type="text" placeholder="Name" name="name" />
+        <input type="email" placeholder="Email" name="email" />
+        <button type="submit">Submit</button>
+      </form>
 
-     <div>
-      {
-        users.map(user => <p key={user.id}>{user.name} {user.email}</p>)
-      }
-     </div>
+      <div>
+        {users.map((user) => (
+          <h3 key={user.id}>
+            {user.name}___{user.email}
+          </h3>
+        ))}
+      </div>
     </div>
   );
 }
